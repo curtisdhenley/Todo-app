f71c5bd59f9b0711d007e4c190da48d445d0b37e
unknown <curtisdhenley@gmail.com>
2022-05-21 19:44:12 -0400

    added input btn functionality

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,35 @@
 // DOM cache
+const todoInput = document.getElementById("todo-input");
+const todoBtn = document.getElementById("todo-button");
+const todoList = document.getElementById("todo-list");
 
 
-// create a function that adds new tasks to todo list
+// event listeners
+todoBtn.addEventListener("click", addTodo);
 
 
-// create new task
-
-
-
-// create function that removes task from Todo
+// functions
+function addTodo(event) {
+    // prevent form from submitting
+    event.preventDefault()
+    // todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = `hey`;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // completed btn
+    const completedBtn = document.createElement('button');
+    completedBtn.innerHTML = `<i class="fas fa-check"></i>`;
+    completedBtn.classList.add('complete-btn');
+    todoDiv.appendChild(completedBtn);
+    // delete btn
+    const deletedBtn = document.createElement('button');
+    deletedBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+    deletedBtn.classList.add('complete-btn');
+    todoDiv.appendChild(deletedBtn);
+    // append to list
+    todoList.appendChild(todoDiv)
+}
